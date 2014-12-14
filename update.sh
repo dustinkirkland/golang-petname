c41654dafefe126bf624ad0a99809bb89058e77b
Dustin Kirkland <dustin.kirkland@gmail.com>
2014-12-14 17:51:15 -0600

    * adverbs.txt, debian/control, debian/petname.install, debian/python-   petname.install, __init__.py, Makefile, names.txt, petname.go.in,   petname/petname.py, petname/petname.py.in, === removed directory   petname, setup.py, update.sh:   - rework to a golang only package

--- update.sh
+++ update.sh
@@ -4,22 +4,12 @@ set -e
 
 PKG="petname"
 
-cp -f ${PKG}.go.in ${PKG}.go.unf
-cp -f ${PKG}/${PKG}.py.in ${PKG}/${PKG}.py.unf
-
-for f in adverbs.txt adjectives.txt names.txt; do
-	rm -f "$f".list
+for f in adverbs adjectives names; do
+	rm -f "$f".txt.list
 	for w in $(cat "$f"); do
-		printf '"%s", ' "$w" >> "$f".list
+		printf '"%s", ' "$w" >> "$f".txt.list
 	done
-	sed -i -e "s/, $//" "$f".list
-	sed -i -e "s/__${f}__/$(cat ${f}.list)/" ${PKG}.go.unf
-	sed -i -e "s/__${f}__/$(cat ${f}.list)/" ${PKG}/${PKG}.py.unf
+	sed -i -e "s/, $//" "$f".txt.list
+	sed -i -e "s/^        $f    = [...]string{.*$/        $f    = [...]string{$(cat ${f}.list)}/" ${PKG}.go.unf
 	rm -f "$f".list
 done
-gofmt ${PKG}.go.unf > ${PKG}.go
-autopep8 ${PKG}/${PKG}.py.unf > ${PKG}/${PKG}.py
-rm -f ${PKG}.go.unf ${PKG}/${PKG}.py.unf
-go build ${PKG}.go
-python -m py_compile ${PKG}/${PKG}.py
-rm -f ${PKG}/*pyc
