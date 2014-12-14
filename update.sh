4bb5a51adf68fd25f160bd02fd2a6e85fbc80639
Dustin Kirkland <dustin.kirkland@gmail.com>
2014-12-14 15:43:00 -0600

    * === added directory petname, debian/python-petname.install,   petname.py.in => petname/petname.py.in, petname.py =>   petname/petname.py, update.sh:   - rearrange dirs for python packaging

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ set -e
 PKG="petname"
 
 cp -f ${PKG}.go.in ${PKG}.go.unf
-cp -f ${PKG}.py.in ${PKG}.py.unf
+cp -f ${PKG}/${PKG}.py.in ${PKG}/${PKG}.py.unf
 
 for f in adverbs.txt adjectives.txt names.txt; do
 	rm -f "$f".list
@@ -14,11 +14,12 @@ for f in adverbs.txt adjectives.txt names.txt; do
 	done
 	sed -i -e "s/, $//" "$f".list
 	sed -i -e "s/__${f}__/$(cat ${f}.list)/" ${PKG}.go.unf
-	sed -i -e "s/__${f}__/$(cat ${f}.list)/" ${PKG}.py.unf
+	sed -i -e "s/__${f}__/$(cat ${f}.list)/" ${PKG}/${PKG}.py.unf
 	rm -f "$f".list
 done
 gofmt ${PKG}.go.unf > ${PKG}.go
-autopep8 ${PKG}.py.unf > ${PKG}.py
-rm -f ${PKG}.go.unf ${PKG}.py.unf
+autopep8 ${PKG}/${PKG}.py.unf > ${PKG}/${PKG}.py
+rm -f ${PKG}.go.unf ${PKG}/${PKG}.py.unf
 go build ${PKG}.go
-python -m py_compile ${PKG}.py
+python -m py_compile ${PKG}/${PKG}.py
+rm -f ${PKG}/*pyc
