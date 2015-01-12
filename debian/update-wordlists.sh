b4fb9cea633a11ef1be5d03bce68f623c1cd5616
Dustin Kirkland <dustin.kirkland@gmail.com>
2015-01-12 09:47:24 -0600

    * debian/update-wordlists.sh:   - update word list location to share

--- debian/update-wordlists.sh
+++ debian/update-wordlists.sh
@@ -6,7 +6,7 @@ PKG="petname"
 
 for f in adverbs adjectives names; do
 	rm -f "$f".txt.list
-	for w in $(cat /usr/lib/petname/"$f".txt); do
+	for w in $(cat /usr/share/petname/"$f".txt); do
 		printf '"%s", ' "$w" >> "$f".txt.list
 	done
 	sed -i -e "s/, $//" "$f".txt.list
